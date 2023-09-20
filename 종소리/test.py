e2813e7bbf7e4bb8e083020b0170bee76d098f6d
정찬빈 <117147980+icerice06@users.noreply.github.com>
2023-09-20 09:57:00 +0900

    Add files via upload

---
+++ 종소리/test.py
@@ -0,0 +1,12 @@
+#import os
+#os.system('pwa')
+#os.system('mpg123 a3.mp3 &')
+
+# times_to_play_sound = []
+# f = open("시정표.txt", 'r')
+# while True:
+#     line = f.readline()
+#     times_to_play_sound = print(line.split())
+#     if not line: break
+
+# print(times_to_play_sound)
