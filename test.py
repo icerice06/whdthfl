f7a75d3ad8f45b8d3ef51999d30dc04f4656451a
정찬빈 <117147980+icerice06@users.noreply.github.com>
2023-09-20 11:12:50 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+#import os
+#os.system('pwd')
+#os.system('mpg123 a3.mp3 &')
+
+# times_to_play_sound = []
+# f = open("tlwjdvy.txt", 'r')
+# while True:
+#     line = f.readline()
+#     times_to_play_sound = print(line.split())
+#     if not line: break
+
+# print(times_to_play_sound)
---
+++ txt.py
@@ -0,0 +1,18 @@
+#pip install playsound==1.2.2
+
+from playsound import playsound
+import time
+
+times_to_play_sound = []
+with open("tlwjdvy.txt", 'r') as f:
+    for line in f:
+        line = line.strip() 
+        times_to_play_sound.extend(line.split())
+
+print("START")
+while True:
+    current_time = time.strftime("%H:%M:%S")
+    
+    if current_time in times_to_play_sound:
+        print(current_time)
+        playsound("a3.mp3")
